41372b35330c8b46fb900d561cc91e9301964c7d
gabo52 <gabrielduranruiz@gmail.com>
2025-03-11 11:26:30 -0500

    video upload working

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,108 +1,12 @@
 "use client";
-import axios from "axios";
-import { useRef, useState } from "react";
-import UploadFile from "./_components/UploadFile";
-import WatchFrames from "./_components/WatchFrames";
-import { sleep } from "@/lib/utils";
-import { useToast } from "@/hooks/use-toast";
-import { MyComposition } from "@/remotion/Composition";
-import { Player } from "@remotion/player";
-import { MyVideo } from "./remotion/MyVideo";
-import { FadeIn } from "@/remotion/FaeIn";
-import { MyComp } from "@/remotion/MyComp";
 
-const CSV_URL = "http://localhost:3000/results.csv";
+import NoSSRWrapper from "./NoSSRWrapper.tsx";
+import Home from "./Home.tsx";
 
-export default function Home() {
-  const [idx, setIdx] = useState(0);
-  const [isVideoSelected, setIsVideoSelected] = useState(false);
-  const [maxLength, setMaxLength] = useState(0);
-  const [labels, setLabels] = useState<Array<string>>([]);
-  const { toast } = useToast();
-
-  const handleCountChange = (newValue: number) => {
-    if (newValue !== maxLength && newValue >= 0) {
-      setIdx(newValue);
-    }
-  };
-
-  const assignLabel = async (label: string) => {
-    labels[idx] = label;
-    setLabels(labels);
-    console.log(labels);
-    toast({
-      description: `${label} label assigned`,
-      variant: `${label}_label`,
-    });
-  };
-
-  const loadVideo = async (videoPath: string) => {
-    // Aca se llama la api en back y se procesan todos los frames del video guardandose en una carpeta
-    // Luego se muestra la imagen que corresponde al indice de la variable count y con ello ya se muestran resultados
-    console.log(`El video path es ${videoPath}`);
-
-    const response = await axios.post("http://localhost:5000/api/video", {
-      video_path: videoPath,
-    });
-
-    if (response.data === 0) {
-      toast({
-        description: `Video file on ${videoPath} not found`,
-        variant: `destructive`,
-      });
-      return;
-    }
-    setIsVideoSelected(true);
-    setVideoPath(videoPath);
-    await sleep(2000);
-    setMaxLength(response.data);
-    const newArray = new Array<string>(response.data);
-    setLabels(newArray);
-  };
-
-  const downloadCSV = async () => {
-    await axios.post("http://localhost:5000/api/video/extractResults", {
-      labels: labels,
-    });
-    const aTag = document.createElement("a");
-    aTag.href = CSV_URL;
-    aTag.setAttribute("download", "labels_classified.csv");
-    document.body.appendChild(aTag);
-    aTag.click();
-    aTag.remove();
-  };
-
+export default function Page() {
   return (
-    <>
-      <div className="min-h-screen bg-slate-200 flex justify-center items-center font-mono">
-        {isVideoSelected ? (
-          <WatchFrames
-            idx={idx}
-            handleCountChange={handleCountChange}
-            assignLabel={assignLabel}
-            downloadCSV={downloadCSV}
-          ></WatchFrames>
-        ) : (
-          <div className="w-[70vw]  m-[5rem 0] bg-blue-300 max-w-[1470px] rounded-sm p-[1.5rem 2rem] border-red-500 shadow-xl">
-            <div>
-              <UploadFile loadVideo={loadVideo}></UploadFile>
-              <Player
-                component={MyComp}
-                inputProps={{ text: "World" }}
-                durationInFrames={120}
-                compositionWidth={1920}
-                compositionHeight={1080}
-                fps={30}
-                style={{
-                  width: 1280,
-                  height: 720,
-                }}
-                controls
-              />
-            </div>
-          </div>
-        )}
-      </div>
-    </>
+    <NoSSRWrapper>
+      <Home />
+    </NoSSRWrapper>
   );
 }
